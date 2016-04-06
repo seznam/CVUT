9f8d3796784dda03747b86c8b635a0f234bd6b35
Zdenek Vlach <zdenek.vlach@firma.seznam.cz>
2016-04-06 07:32:20 +0200

    komentar

--- A0M33KAJ/cviceni/07/visual.js
+++ A0M33KAJ/cviceni/07/visual.js
@@ -3,10 +3,10 @@ var Visual = function () {
 	this._dom = document.createElement('div');
 	this._dom.classList.add("infoBox");
     document.body.appendChild(this._dom);
-    this._dom.addEventListener('click', this._show.bind(this),true);
+    //this._dom.addEventListener('click', this._show.bind(this),true);
 }
 
 Visual.prototype._show = function (message) {
 	console.log(this);
-	this._dom.innerHTML = this.message;
+	this._dom.innerHTML = message;
 }
